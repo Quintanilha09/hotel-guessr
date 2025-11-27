451dae6180f96405d26862ba1a0ea8ea52d1ed48
GABRIEL QUINTANILHA CABRAL DE OLIVEIRA <gabriel.c.oliveira+bradesco@bradesco.com.br>
2025-11-26 21:07:56 -0300

    feat - Implementação real de retorno de hotéis próximos

---
+++ src/main/java/com/hotel/transilvania/dto/GeocodingResponse.java
@@ -0,0 +1,39 @@
+package com.hotel.transilvania.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GeocodingResponse {
+    
+    private List<Result> results;
+    private String status;
+    
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Result {
+        private Geometry geometry;
+    }
+    
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Geometry {
+        private Location location;
+    }
+    
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Location {
+        private Double lat;
+        private Double lng;
+    }
+}
