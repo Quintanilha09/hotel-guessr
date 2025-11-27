8aaac38f0a04493804a383ef39b19cf7065cbd1f
GABRIEL QUINTANILHA CABRAL DE OLIVEIRA <gabriel.c.oliveira+bradesco@bradesco.com.br>
2025-11-27 10:48:31 -0300

    feat - Criação de properties para o docker e .env para a api key

--- src/test/java/com/hotel/guessr/HotelGuessrApplicationTests.java
+++ src/test/java/com/hotel/guessr/HotelGuessrApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HotelGuessrApplicationTests {
 
-@Test
-void contextLoads() {
-}
+	@Test
+	void contextLoads() {
+	}
 
 }
--- src/test/java/com/hotel/guessr/TransilvaniaApplicationTests.java
+++ src/test/java/com/hotel/guessr/TransilvaniaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class guessrApplicationTests {
+class HotelGuessrIntegrationTests {
 
 	@Test
 	void contextLoads() {
